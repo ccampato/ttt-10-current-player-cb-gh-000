d873887caab59fa743cf9bb8796fe0879de0bcb6
Caio Sanches <ccampato@gmail.com>
2018-04-07 22:45:34 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,10 @@
 board = [" "," "," "," "," "," "," "," "," "]
 count = 0
-def turn_count(board, count)
-  if board[count] != " "
-    count += 1
+
+def turn_count(board)
+  board.each do |occupied|
+    if occupied != " "
+      count += 1
+    end
   end
 end
